98d71551e3a350596ba9b0de456e9d993653597c
Bhupendra <bhupendra_m@cs.iitr.ac.in>
2022-11-23 11:46:55 +0530

    fixes

--- client/src/components/NoAvatar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../index.css'
-
-export default function NoAvatar({letter}) {
-  return (
-    <div className='d-flex text-center noavatar'>{letter.toUpperCase()}</div>
-  )
-}
